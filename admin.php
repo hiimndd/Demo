3f2e19c71fd73859891b56eb588ae0858855fb36
hiimndd <hiimndd@gmail.com>
2021-01-20 14:54:12 +0700

    update all

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+sort($cars);
+
+foreach($cars as $b){
+  echo $b."<br>";
+}
+
+$car = "Chào bạn";
+echo $car;
+?>
+
+</body>
+</html>
